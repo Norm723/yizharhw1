16c3edd3a8033fce0f92fdb426e8084e7a1334d8
Norm723 <norm.twersky@gmail.com>
2020-04-09 23:27:13 +0300

    Add files via upload

--- hw1/question2/main_faithful.m
+++ hw1/question2/main_faithful.m
@@ -44,10 +44,10 @@ theta1=linspace(0, 20, 500);
 J =zeros(length(theta0),length(theta1));
 % a matrix of J values for each theta
 for i = 1:length(theta0)
-for j = 1:length(theta1)
-thetamatrix = [theta0(i); theta1(j)];
-J(i,j) = computeCost(X, y, thetamatrix);
-end
+    for j = 1:length(theta1)
+        thetamatrix = [theta0(i); theta1(j)];
+        J(i,j) = computeCost(X, y, thetamatrix);
+    end
 end
 % contour plot using a logarithmic scale
 contour(theta0, theta1, J, logspace(-2, 7, 35))
@@ -55,3 +55,5 @@ xlabel('\theta_0'); ylabel('\theta_1');
 hold on;
 plot(theta(1), theta(2), 'bx', 'MarkerSize', 5, 'LineWidth', 2);
 % theta(1) and theta(2) are the values computed by gradient descent
+
+%% 6 see pdf
